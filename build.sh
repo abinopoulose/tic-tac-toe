7a33c58537cc9c85720186cdd3104a0c449423ff
Abino Poulose <abinopoulose@outlook.com>
2025-06-03 23:51:50 +0530

    release @ 2025-06-03 23:51:40

---
+++ build.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+set -e  # Exit on any error
+
+#clean 
+git checkout release
+rm -rf assets/ index.html vite.svg || true
+rm -rf tic-tac-toe || true
+
+#build
+git clone git@github.com:abinopoulose/tic-tac-toe.git
+cd tic-tac-toe
+git checkout main
+git pull
+
+npm install
+npm run build
+cd ..
+
+#copy build files correct path
+cp -r tic-tac-toe/dist/* .
+sed -i 's|assets|tic-tac-toe/assets|g' index.html assets/*
+
+
+# commit and push
+rm -rf tic-tac-toe || true
+# git add .
+# DATETIME=$(date +"%Y-%m-%d %H:%M:%S")
+# git commit -m "release @ $DATETIME"
+# git push
+
+
+# echo "script completed successfully!"
+
